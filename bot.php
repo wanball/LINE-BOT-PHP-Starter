d802aa471e7e6affd28c96841c82fdb2338d1a66
wanball <wan.ball@gmail.com>
2017-01-06 14:11:09 +0700

    test12

--- bot.php
+++ bot.php
@@ -50,9 +50,23 @@ echo engtothai('abaft');
 echo '---------------------------';
 function engtothai($text){
 	
-$url = 'http://campaignactivity.com/demo/index.php?p='.$text;
-$ch = curl_init($url);
-$ret = curl_exec($ch);
-curl_close($ch);
-return $text.' '.$ret;
+$url = 'http://campaignactivity.com/demo/index.php';
+$data = 'p='.$text;
+try{
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_URL, $url );
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+    curl_setopt( $ch, CURLOPT_POST, true );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+    $content = curl_exec( $ch );
+    curl_close($ch);
+    
+    return $content;
+    
+  }catch(Exception $ex){
+  
+    echo $ex;
+  }
+
 }
